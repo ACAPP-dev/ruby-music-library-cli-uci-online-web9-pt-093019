921014ff6362a5576b4e200c58e4ec88aca186e0
Andrew Capp <acapp909@gmail.com>
2019-11-15 23:49:54 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -67,7 +67,7 @@ class MusicLibraryController
         response = gets.chomp
         if Genre.all.detect {|genre| genre.name == response}
           alpha_genres = Genre.find_by_name(response).songs.sort {|song1, song2| song1.name <=> song2.name}
-          alpha_songs.each.with_index(1) do |song, index|
+          alpha_genres.each.with_index(1) do |song, index|
             puts "#{index}. #{song.name} - #{song.genre.name}"
           end
         end
